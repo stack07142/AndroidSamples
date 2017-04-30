ef6a526deb5849b94b9f9c7fb7147a281ec6e323
stack07142 <stack.07142@gmail.com>
2017-04-30 12:46:09 +0900

    Firebase Demo

---
+++ Sample_Firebase_DB/app/src/main/java/com/android/sample_firebase_db/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.sample_firebase_db;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
