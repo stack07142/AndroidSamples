7347a4b1126d91cf6235b27444cba547d5e5f2b6
stack07142 <stack.07142@gmail.com>
2017-07-23 15:45:17 +0900

    구글로 로그인 예제

--- Sample_Firebase_auth/app/src/main/java/com/android/sample_firebase_auth/LoginActivity.java
+++ Sample_Firebase_auth/app/src/main/java/com/android/sample_firebase_auth/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,13 +15,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
-    private static final int RC_SIGN_IN = 9001;
 
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInOptions gso;
     private GoogleApiClient mGoogleApiClient;
+
+    // View
+    private TextView retTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,13 @@ public class LoginActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate()");
 
-        // [START configure_signin]
+        // View
+        retTextView = (TextView) findViewById(R.id.ret_textview);
+
+        // Firebase 로그인 통합 관리하는 Object 만들기
+        mAuth = FirebaseAuth.getInstance();
+
+        // 구글 로그인 옵션 설정 (요청 토큰, 요청 권한.. 등등)
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -37,12 +55,15 @@ public class LoginActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        // 구글 로그인 API 만들기
         // Build a GoogleApiClient with access to the Google Sign-In API and the
         // options specified by gso.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+
                     @Override
                     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
                         Log.d(TAG, "Login fail");
                     }
                 })
@@ -77,18 +98,53 @@ public class LoginActivity extends AppCompatActivity {
             if (result.isSuccess()) {
                 // 로그인 성공 했을때
                 GoogleSignInAccount acct = result.getSignInAccount();
-                Log.d(TAG, "표시되는 전체 이름 =" + acct.getDisplayName());
-                Log.d(TAG, "표시되는 이름=" + acct.getGivenName());
-                Log.d(TAG, "이메일=" + acct.getEmail());
-                Log.d(TAG, "표시되는 성=" + acct.getFamilyName());
 
+                firebaseAuthWithGoogle(acct);
             } else {
 
-                Log.e(TAG, "로그인 실패");
-                // 로그인 실패 했을때
+
             }
         }
+    }
 
+    // 구글에서 받는 정보를 파이어베이스 서버로 넘기기
+    private void firebaseAuthWithGoogle(final GoogleSignInAccount acct) {
+
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            String retText = "표시되는 전체 이름 =" + acct.getDisplayName()
+                                    + "\n표시되는 이름=" + acct.getGivenName()
+                                    + "\n이메일=" + acct.getEmail()
+                                    + "\n표시되는 성=" + acct.getFamilyName();
+
+                            retTextView.setText(retText);
+
+                            Log.d(TAG, retText);
+
+                        } else {
+
+                            // 로그인 실패 했을때
+                            Log.e(TAG, "로그인 실패");
+
+                            retTextView.setText("로그인 실패");
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
