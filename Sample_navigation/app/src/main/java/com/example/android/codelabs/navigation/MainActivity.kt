96d2ab1d6fa93673e611d0f258074a0a2f428225
stack07142 <stack.07142@gmail.com>
2019-06-11 01:26:15 +0900

    Using NavigationUI to configure Bottom Navigation - Set up a BottomNavigationView for use with a NavController - This will android.view.MenuItem.onNavDestinationSelected when a menu item is selected - Notice that their ids match the destinations of navigation graph

--- Sample_navigation/app/src/main/java/com/example/android/codelabs/navigation/MainActivity.kt
+++ Sample_navigation/app/src/main/java/com/example/android/codelabs/navigation/MainActivity.kt
@@ -24,8 +24,12 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.navigation_activity.*
 
@@ -33,7 +37,7 @@ import kotlinx.android.synthetic.main.navigation_activity.*
  * A simple activity demonstrating use of a NavHostFragment with a navigation drawer.
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,9 +46,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val host: NavHostFragment = supportFragmentManager
-                .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
-
-        // Set up Action Bar
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
         val navController = host.navController
 
         //appBarConfiguration = AppBarConfiguration(navController.graph)
@@ -61,7 +63,13 @@ class MainActivity : AppCompatActivity() {
 
         //setupNavigationMenu(navController)
 
-        //setupBottomNavMenu(navController)
+        /**
+         * Sets up a [BottomNavigationView] for use with a [NavController].
+         * This will call [android.view.MenuItem.onNavDestinationSelected] when a menu item is selected.
+         *
+         * Notice that their ids match the destinations of navigation graph
+         */
+        bottom_nav_view?.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val dest: String = try {
@@ -70,17 +78,12 @@ class MainActivity : AppCompatActivity() {
                 Integer.toString(destination.id)
             }
 
-            Toast.makeText(this@MainActivity, "Navigated to $dest",
-                    Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this@MainActivity, "Navigated to $dest",
+                Toast.LENGTH_SHORT
+            ).show()
             Log.d("NavigationActivity", "Navigated to $dest")
         }
-    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
-        // TODO STEP 9.3 - Use NavigationUI to set up Bottom Nav
-//        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-//        bottomNav?.setupWithNavController(navController)
-        // TODO END STEP 9.3
     }
 
     private fun setupNavigationMenu(navController: NavController) {
@@ -92,8 +95,10 @@ class MainActivity : AppCompatActivity() {
         // TODO END STEP 9.4
     }
 
-    private fun setupActionBar(navController: NavController,
-                               appBarConfig : AppBarConfiguration) {
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
         // TODO STEP 9.6 - Have NavigationUI handle what your ActionBar displays
 //        // This allows NavigationUI to decide what label to show in the action bar
 //        // By using appBarConfig, it will also determine whether to
@@ -115,15 +120,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-        // TODO STEP 9.2 - Have Navigation UI Handle the item selection - make sure to delete
-        //  the old return statement above
-//        // Have the NavigationUI look for an action or destination matching the menu
-//        // item id and navigate there if found.
-//        // Otherwise, bubble up to the parent.
-//        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
-//                || super.onOptionsItemSelected(item)
-        // TODO END STEP 9.2
+        // Have the NavigationUI look for an action or destination matching the menu
+        // item id and navigate there if found.
+        // Otherwise, bubble up to the parent.
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 
     // TODO STEP 9.7 - Have NavigationUI handle up behavior in the ActionBar
