0693cd5583b72d15efa02c9f2434e538d5bdb481
stack.07142 <stack.07142@gmail.com>
2019-06-18 22:10:05 +0900

    ble scan

--- Sample_BLE/app/src/main/java/k/t/sample_ble/ble/BLEUtils.kt
+++ Sample_BLE/app/src/main/java/k/t/sample_ble/ble/BLEUtils.kt
@@ -1,7 +1,71 @@
 package k.t.sample_ble.ble
 
-object BLEUtils {
-    fun startScan() {
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.*
+import android.os.ParcelUuid
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposables
+import org.apache.commons.lang3.reflect.MethodUtils
+import timber.log.Timber
+import java.lang.ref.WeakReference
 
+object BLEUtils {
+    fun startScan(): Observable<List<ScanResult>> {
+        return Observable.create { e ->
+            val filters = ScanFilter.Builder().apply {
+                setServiceUuid(ParcelUuid(BLEProfile.SERVICE_UUID))
+            }.build()
+
+            val settings = ScanSettings.Builder().apply {
+                setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                setReportDelay(2000L)
+            }.build()
+
+            val callback = object : ScanCallback() {
+                private val refEmitter = WeakReference(e)
+
+                override fun onScanResult(callbackType: Int, result: ScanResult) {
+                    Timber.tag("BLE SCANNER").d("onScanResult : %d %s", callbackType, result)
+                }
+
+                override fun onBatchScanResults(results: List<ScanResult>) {
+                    Timber.tag("BLE SCANNER").d("onBatchScanResults(count) : %d", results.size)
+                    onNext(results)
+                }
+
+                override fun onScanFailed(errorCode: Int) {
+                    Timber.tag("BLE SCANNER").e("onScanFailed %d", errorCode)
+                    tryOnError(IllegalStateException("onScanFailed $errorCode"))
+                }
+
+                private fun onNext(results: List<ScanResult>) {
+                    val emitter = refEmitter.get()
+                    emitter?.onNext(results)
+                }
+
+                private fun tryOnError(error: Throwable) {
+                    val emitter = refEmitter.get()
+                    emitter?.tryOnError(error)
+                }
+            }
+
+            val scanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+            scanner.startScan(listOf(filters), settings, callback)
+
+            e.setDisposable(Disposables.fromAction {
+                Timber.d("stopScan")
+                scanner.stopScan(callback)
+                cleanup(scanner)
+            })
+        }
+    }
+
+    private fun cleanup(scanner: BluetoothLeScanner) {
+        try {
+            Timber.d("cleanup")
+            MethodUtils.invokeMethod(scanner, true, "cleanup")
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
     }
 }
--- Sample_BLE/app/src/main/java/k/t/sample_ble/fragments/CentralFragment.kt
+++ Sample_BLE/app/src/main/java/k/t/sample_ble/fragments/CentralFragment.kt
@@ -1,5 +1,6 @@
 package k.t.sample_ble.fragments
 
+import android.bluetooth.le.ScanResult
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,24 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.trello.rxlifecycle3.components.support.RxFragment
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import k.t.sample_ble.R
+import k.t.sample_ble.ble.BLEUtils
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.central_fragment.*
 import kotlinx.android.synthetic.main.listitem_device.*
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
-private const val SCAN_PERIOD: Long = 5000L
+private const val SCAN_PERIOD: Long = 20L
 
 class CentralFragment : RxFragment() {
+    private var scanDisposable: Disposable? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.central_fragment, container, false)
     }
@@ -23,18 +34,43 @@ class CentralFragment : RxFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val scanResultAdapter = ScanResultAdapter()
         rvScanList.apply {
             setHasFixedSize(true)
-            adapter = ScanResultAdapter()
+            adapter = scanResultAdapter
         }
 
-        btnScan.setOnClickListener {
-
+        btnScan.text = getString(R.string.btn_scan, SCAN_PERIOD)
+        btnScan.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                scanDisposable = BLEUtils.startScan()
+                    .takeUntil(Observable.timer(SCAN_PERIOD, TimeUnit.SECONDS))
+                    .compose(bindToLifecycle())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy(
+                        onNext = {
+                            it.forEach { scanResult ->
+                                Timber.d("${scanResult.device.name} / ${scanResult.device.address}")
+                            }
+                            scanResultAdapter.submitList(it)
+                        },
+                        onComplete = {
+                            Timber.d("BLE startScan-onComplete")
+                            btnScan.isChecked = false
+                        },
+                        onError = {
+                            Timber.e(it)
+                        }
+                    )
+            } else {
+                scanDisposable?.dispose()
+            }
         }
     }
 }
 
-class ScanResultAdapter : ListAdapter<ScanResultAdapter.Item, ScanResultAdapter.ItemViewHolder>(ItemCallback()) {
+class ScanResultAdapter : ListAdapter<ScanResult, ScanResultAdapter.ItemViewHolder>(ItemCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.listitem_device, parent, false)
         return ItemViewHolder(view)
@@ -43,22 +79,20 @@ class ScanResultAdapter : ListAdapter<ScanResultAdapter.Item, ScanResultAdapter.
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         getItem(position)?.let {
             with(holder) {
-                tvDeviceName.text = it.name
-                tvDeviceAddress.text = it.address
+                tvDeviceName.text = it.device.name ?: "null"
+                tvDeviceAddress.text = it.device.address
             }
         }
     }
 
     class ItemViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
 
-    data class Item(val name: String, val address: String)
-
-    class ItemCallback : DiffUtil.ItemCallback<Item>() {
-        override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean =
+    class ItemCallback : DiffUtil.ItemCallback<ScanResult>() {
+        override fun areItemsTheSame(oldItem: ScanResult, newItem: ScanResult): Boolean =
             areContentsTheSame(oldItem, newItem)
 
-        override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean =
-            oldItem.name == newItem.name && oldItem.address == newItem.address
+        override fun areContentsTheSame(oldItem: ScanResult, newItem: ScanResult): Boolean =
+            oldItem.device.address == newItem.device.address
     }
 }
 
